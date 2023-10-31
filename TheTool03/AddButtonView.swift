ba6073be5a6b69e07d9dbbacedf5761130e5ab91
Achraf Kassioui <achrafkassioui@gmail.com>
2023-11-01 00:24:06 +0100

    Fixed indentation

--- TheTool03/AddButtonView.swift
+++ TheTool03/AddButtonView.swift
@@ -2,45 +2,45 @@ import SwiftUI
 import SpriteKit
 
 struct AddButtonView: View {
-	@State private var dragOffset = CGSize.zero
-	@State private var isDragging = false
-	@State private var releasePosition: CGPoint?
-	
-	var body: some View {
-		let drag = DragGesture()
-			.onChanged { value in
-				self.dragOffset = value.translation
-				self.isDragging = true
-			}
-			.onEnded { value in
-				print(value.location)
-				self.isDragging = false
-				// self.releasePosition = ?
-				withAnimation(.spring(response: 0.3, dampingFraction: 0.7)) {
-					self.dragOffset = .zero
-				}
-				
-				// Check if the button has been released over a specific view (SpriteView)
-				// Here, you'd use the releasePosition to calculate whether it's within the bounds of your target view
-				// For now, I'm using a simple print statement for demonstration purposes
-			}
-		
-		let tap = TapGesture()
-			.onEnded {
-				// This will be triggered if the view is simply tapped (and not dragged)
-				if !isDragging {
-					print("Button was tapped!")
-					// Trigger floating menu display logic here
-				}
-			}
-		
-		Image(systemName: "plus")
-			.font(.system(size: 25))
-			.padding()
-			.background(Circle().fill(Color.white).overlay(Circle().stroke(Color.black, lineWidth: 2)))
-			.offset(dragOffset)
-			.gesture(tap)
-			.gesture(drag)
-	}
+    @State private var dragOffset = CGSize.zero
+    @State private var isDragging = false
+    @State private var releasePosition: CGPoint?
+    
+    var body: some View {
+        let drag = DragGesture()
+            .onChanged { value in
+                self.dragOffset = value.translation
+                self.isDragging = true
+            }
+            .onEnded { value in
+                print(value.location)
+                self.isDragging = false
+                // self.releasePosition = ?
+                withAnimation(.spring(response: 0.3, dampingFraction: 0.7)) {
+                    self.dragOffset = .zero
+                }
+                
+                // Check if the button has been released over a specific view (SpriteView)
+                // Here, you'd use the releasePosition to calculate whether it's within the bounds of your target view
+                // For now, I'm using a simple print statement for demonstration purposes
+            }
+        
+        let tap = TapGesture()
+            .onEnded {
+                // This will be triggered if the view is simply tapped (and not dragged)
+                if !isDragging {
+                    print("Button was tapped!")
+                    // Trigger floating menu display logic here
+                }
+            }
+        
+        Image(systemName: "plus")
+            .font(.system(size: 25))
+            .padding()
+            .background(Circle().fill(Color.white).overlay(Circle().stroke(Color.black, lineWidth: 2)))
+            .offset(dragOffset)
+            .gesture(tap)
+            .gesture(drag)
+    }
 }
 
--- TheTool03/BackgroundGrid.swift
+++ TheTool03/BackgroundGrid.swift
@@ -2,20 +2,20 @@ import Foundation
 import SpriteKit
 
 extension Composition {
-	func createBackgroundFromImage() {
-		let gridPatternTexture = SKTexture(imageNamed: "gridPattern")
-		let xRepeats = Int(size.width / gridPatternTexture.size().width)
-		let yRepeats = Int(size.height / gridPatternTexture.size().height)
-		for i in -xRepeats/2..<xRepeats/2 {
-			for j in -yRepeats/2..<yRepeats/2 {
-				let backgroundPattern = SKSpriteNode(texture: gridPatternTexture)
-				backgroundPattern.position = CGPoint(
-					x: CGFloat(i) * gridPatternTexture.size().width,
-					y: CGFloat(j) * gridPatternTexture.size().height
-				)
-				backgroundPattern.anchorPoint = CGPoint(x: 0, y: 0)
-				addChild(backgroundPattern)
-			}
-		}
-	}
+    func createBackgroundFromImage() {
+        let gridPatternTexture = SKTexture(imageNamed: "gridPattern")
+        let xRepeats = Int(size.width / gridPatternTexture.size().width)
+        let yRepeats = Int(size.height / gridPatternTexture.size().height)
+        for i in -xRepeats/2..<xRepeats/2 {
+            for j in -yRepeats/2..<yRepeats/2 {
+                let backgroundPattern = SKSpriteNode(texture: gridPatternTexture)
+                backgroundPattern.position = CGPoint(
+                    x: CGFloat(i) * gridPatternTexture.size().width,
+                    y: CGFloat(j) * gridPatternTexture.size().height
+                )
+                backgroundPattern.anchorPoint = CGPoint(x: 0, y: 0)
+                addChild(backgroundPattern)
+            }
+        }
+    }
 }
